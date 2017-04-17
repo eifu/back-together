8712cc772cddbf5609d229ff56945c98c440614f
cwong2015 <sirhc8002@gmail.com>
2017-04-16 20:17:02 -0400

    removed unnecessary global variables

--- public/js/MainMenu.js
+++ public/js/MainMenu.js
@@ -1,15 +1,7 @@
 BackTogether.MainMenu = function (game) {
 
 };
-var play;
-var menuLayer;
-var helpLayer;
-var settings;
-var help;
-var helpLayerCard;
-var helpLayer;
-var helpTxt;
-var helpTxt2;
+
 var map;
 
 var wfconfig = {
@@ -26,7 +18,7 @@ BackTogether.MainMenu.prototype = {
         map = this.add.tilemap('map', 64, 64);
         map.addTilesetImage('tileset');
 
-        menuLayer = map.createLayer('pausedLayer');
+        var menuLayer = map.createLayer('pausedLayer');
         // menuLayer.resizeWorld();
         menuLayer.alpha = 0.6;
         
@@ -37,29 +29,29 @@ BackTogether.MainMenu.prototype = {
         
         txt.anchor.setTo(0.5, 0.5);
 
-        play = game.add.button(game.world.centerX, game.world.centerY-70, 'mainMenuBtn', startOnClick, this, 2, 1, 0);
-        play.anchor.setTo(0.5, 0.5);
+        var playBtn = game.add.button(game.world.centerX, game.world.centerY-70, 'mainMenuBtn', startOnClick, this, 2, 1, 0);
+        playBtn.anchor.setTo(0.5, 0.5);
         
-        settings = game.add.button(game.world.centerX, game.world.centerY, 'mainMenuBtn', showSettings, this, 2, 1, 0);
-        settings.anchor.setTo(0.5, 0.5);        
+        var settingsBtn = game.add.button(game.world.centerX, game.world.centerY, 'mainMenuBtn', showSettings, this, 2, 1, 0);
+        settingsBtn.anchor.setTo(0.5, 0.5);        
         
-        help = game.add.button(game.world.centerX, game.world.centerY+70, 'mainMenuBtn', showHelp, this, 2, 1, 0);
-        help.anchor.setTo(0.5, 0.5);
+        var helpBtn = game.add.button(game.world.centerX, game.world.centerY+70, 'mainMenuBtn', showHelp, this, 2, 1, 0);
+        helpBtn.anchor.setTo(0.5, 0.5);
         
 
-        menuTxt = game.add.text(game.world.centerX, game.world.centerY-70, "Play", {
+        var menuTxt = game.add.text(game.world.centerX, game.world.centerY-70, "Play", {
             font: '20px Aclonica', fill: "#F00",
             align: "center"
         });
         menuTxt.anchor.setTo(0.5, 0.5);
 
-        menuTxt2 = game.add.text(game.world.centerX, game.world.centerY, "Settings", {
+        var menuTxt2 = game.add.text(game.world.centerX, game.world.centerY, "Settings", {
             font: '20px Aclonica', fill: "#F00",
             align: "center"
         });
         menuTxt2.anchor.setTo(0.5, 0.5);
         
-        menuTxt3 = game.add.text(game.world.centerX, game.world.centerY + 70, "Help", {
+        var menuTxt3 = game.add.text(game.world.centerX, game.world.centerY + 70, "Help", {
         font: '20px Aclonica', fill: "#F00",
         align: "center"
         });
@@ -75,28 +67,28 @@ BackTogether.MainMenu.prototype = {
         
         function showHelp(){
 
-        helpLayer = map.createLayer('pausedLayer');
+        var helpLayer = map.createLayer('pausedLayer');
         // menuLayer.resizeWorld();
         helpLayer.alpha = 0.4;
 
-        helpLayerCard = game.add.button(game.world.centerX, game.world.centerY, 'menuBtnCard', exitHelp, this, 2, 1, 0);
-        helpLayerCard.anchor.setTo(0.5, 0.5);
-        helpLayerCard.scale.setTo(2,1.5);
+        var helpBtn = game.add.button(game.world.centerX, game.world.centerY, 'menuBtnCard', exitHelp, this, 2, 1, 0);
+        helpBtn.anchor.setTo(0.5, 0.5);
+        helpBtn.scale.setTo(2,1.5);
 
-        helpTxt = game.add.text(game.world.centerX, game.world.centerY, "Press to exit help screen", {
+        var helpTxt = game.add.text(game.world.centerX, game.world.centerY, "Press to exit help screen", {
             font: '20px Aclonica', fill: "#F00",
             align: "center"
         });
         helpTxt.anchor.setTo(0.5, 0.5);
 
-        helpTxt2 = game.add.text(game.world.centerX, game.world.centerY + 75, "←:left \n →:right \n ↑:jump \n ↓:hold item or drop item", {
+        var helpTxt2 = game.add.text(game.world.centerX, game.world.centerY + 75, "←:left \n →:right \n ↑:jump \n ↓:hold item or drop item", {
             font: '20px Aclonica', fill: "#F00",
             align: "center"
         });
         helpTxt2.anchor.setTo(0.5, 0.5);
         
         function exitHelp(){
-            helpLayerCard.destroy();
+            helpBtn.destroy();
             helpLayer.destroy();
             helpTxt.destroy();
             helpTxt2.destroy();
